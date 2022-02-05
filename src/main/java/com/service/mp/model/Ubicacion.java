367249bc94c1804803da87235cbfb24541c418a0
Renato Ayau <renato.ayau@gmail.com>
2022-02-05 16:52:42 -0600

    feat: create api mp ubicacion and fiscalia

---
+++ src/main/java/com/service/mp/model/Ubicacion.java
@@ -0,0 +1,53 @@
+package com.service.mp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "ubicacion")
+public class Ubicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotBlank(message = "Nombre de ubicacion obligatorio")
+    @Length(min = 0, max = 100, message = "Nombre de ubicacion no puede tener mas de 100 caracteres")
+    private String name;
+
+    Ubicacion() {
+    }
+
+    Ubicacion(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ubicacion [id=" + id + ", name=" + name + "]";
+    }
+
+}
---
+++ src/main/java/com/service/mp/repository/UbicacionRepository.java
@@ -0,0 +1,7 @@
+package com.service.mp.repository;
+
+import com.service.mp.model.Ubicacion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UbicacionRepository extends CrudRepository<Ubicacion, Integer> {
+}
